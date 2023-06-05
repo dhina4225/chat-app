efc7e8c4fcea653597f909ace1b507f885d8286b
Dina <Dinakaran0651@gmail.com>
2023-06-05 10:45:07 +0530

    profile update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
-import { MdClose } from "react-icons/md"
+import { MdClose } from "react-icons/md";
+import profile from "../images/profile.png"
 
-const Profile = (setShowProfile) => {
-    return (
-        <div className="layer-opacity">
+const Profile = (setShowProfile) => (
+    <div className="layer-opacity">
         <div className='profile-container'>
-            <span className='close-btn' onClick={()=>setShowProfile(false)}>
-                <MdClose />
-                <span>close</span>
-            </span>
             <div className='form-control'>
-                <img src="" alt="" />
+                <span className='close-btn' onClick={() => setShowProfile (false)}>
+                    <MdClose />
+                    <span>close</span>
+                </span>
+                <img src={profile} alt="" />
                 <div className='form-group'>
                     <label For="first-name">First-Name:</label>
-                        <input type="text" placeholder='First Name' />
+                    <input type="text" placeholder='First Name' />
                     <label For="first-name">Last-Name:</label>
-                        <input type="text" placeholder='Last Name' />
-                </div>
-                <div className='form-group'>
-                    <label For="email">E-Mail:
-                        <input type="e-mail" name='email'/></label>
+                    <input type="text" placeholder='Last Name' />
+                    <label For="email">E-Mail:</label>
+                        <input type="e-mail" name='email' />
                     <label For="password">Password:
                     </label>
-                        <input type="password" name='password'/>
-                </div>
+                    <input type="password" name='password' />
                 <label For="phone-number">Phone-Number:</label>
                     <input type="number" placeholder='Phone Number' />
-                <button className='save-btn'>Save Changes</button>
+                    
+                    <button className='save-btn'>Save Changes</button>
+                </div>
+                    
             </div>
-            </div>
-            </div>
-    );
-}
+        </div>
+    </div>
+)
 
 export default Profile;
